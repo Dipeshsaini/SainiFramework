65b3883b6339379b013c42e1f789e86a9db9d943
Dipesh Saini <mannusaini0001@gmail.com>
2019-11-15 18:56:34 +0530

    initial commit

---
+++ Example/Pods/Target Support Files/SainiFramework/SainiFramework-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SainiFramework : NSObject
+@end
+@implementation PodsDummy_SainiFramework
+@end
---
+++ SainiFramework/Classes/DemoClass.swift
@@ -0,0 +1,21 @@
+//
+//  DemoClass.swift
+//  Pods-SainiFramework_Example
+//
+//  Created by Rumooz Technologies on 15/11/19.
+//
+
+import Foundation
+import UIKit
+
+public class RoundImage:UIImageView
+{
+    override public func layoutSublayers(of layer: CALayer) {
+        super.layoutSublayers(of: layer)
+        layer.cornerRadius = bounds.width/2
+        clipsToBounds = true
+    }
+    override public func prepareForInterfaceBuilder() {
+        backgroundColor = .green
+    }
+}
